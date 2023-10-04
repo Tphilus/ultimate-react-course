efd5801bfa34dbbf4dfacbd5be59cae18446390d
Tphilus <tkdadzie@st.ug.edu.gh>
2023-10-04 00:36:29 +0000

    feat: component, Header, Menu and Footer

--- 03-pizza-menu/src/App.js
+++ 03-pizza-menu/src/App.js
@@ -1,12 +1,98 @@
 import React from 'react';
-import './App.css';
+import './index.css';
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React!!!</h1>
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
+}
+
+const Header = () => {
+  // const style = {color:"red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+  return <header className='header'>
+    <h1 style={style}>Fast React Oizza Co.</h1>
+  </header>
+}
+
+const Menu = () => {
+  return <main className='menu'>
+    <h2>Our menu</h2>
+    <Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, spinach, and ricotta cheese' photoName='pizzas/spinaci.jpg' price={10} />
+    <Pizza name='Pizza Funghi ' ingredients='Tomato, mushrooms' photoName='pizzas/funghi.jpg' price={12} />
+  </main>
+}
+
+const Pizza = (props) => {
+  return <div className='pizza'>
+    <img src={props.photoName} alt={props.name} />
+    <div>
+      <h3> {props.name} </h3>
+      <p>{ props.ingredients }</p>
+      <span>{props.price + 3}</span>
+    </div>
+  </div>
+}
+
+const Footer = () => {
+  const hour = new Date().getHours;
+  const openHour = 8;
+  const closeHour = 22;
+  const isHour = hour >= openHour && hour <= closeHour;
+  console.log(isHour);
+
+  // if(hour >= openHour && hour <= closeHour) alert("We´re currently open!"); else alert("Sorry we´re close")
+  return <footer className='footer'>{new Date().toLocaleTimeString()}.We'r currently open</footer>
 }
 
 export default App;
