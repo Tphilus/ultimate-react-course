7769f3ad8d8b7afc4abae260da6a9c6116bb355a
Tphilus <tkdadzie@st.ug.edu.gh>
2023-10-16 15:59:38 +0000

    Update

--- travel/src/App.js
+++ travel/src/App.js
@@ -25,16 +25,27 @@ const Logo = () => {
 
 const Form = () => {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(5);
+  const [quantity, setQuantity] = useState(1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1)?.map((num) => (
           <option value={num} key={num}>
             {num}
@@ -46,7 +57,7 @@ const Form = () => {
         placeholder="Item..."
         value={description}
         onChange={(e) => {
-          console.log(e.target.value);
+          // console.log(e.target.value);
           setDescription(e.target.value);
         }}
       />
@@ -60,7 +71,7 @@ const PackingList = () => {
     <div className="list">
       <ul>
         {initialItems?.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
