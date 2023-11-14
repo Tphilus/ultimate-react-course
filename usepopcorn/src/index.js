7f38712d2065488c7243dbee3f319710ff06cc9d
Tphilus <tkdadzie@st.ug.edu.gh>
2023-11-14 22:11:26 +0000

    Feat: Update PropsType

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p> This movie was rated {movieRating} stars </p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      size={24}
+      maxRating={5}
+      color="red"
+      className="test"
+      defaultRating={2}
+    />
+
+    <Test />
   </React.StrictMode>
 );
