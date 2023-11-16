cf2dea9141c5876b6df298fcbc066bd5d7f04e2f
Tphilus <tkdadzie@st.ug.edu.gh>
2023-11-16 20:06:23 +0000

    feat: fetch request using fetec

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -51,8 +51,14 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  const KEY = "5f3a1d29";
+
+  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+    .then((res) => res.json())
+    .then((data) => setMovies(data.Search));
 
   return (
     <>
